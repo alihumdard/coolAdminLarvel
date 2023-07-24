20285713c7a980f7f19c9b93d3027525c96c08f6
sanader <sanaderali007@gmail.com>
2023-07-25 00:42:09 +0500

    hhhh

---
+++ resources/views/test.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.main')
+@section('title', 'Tab')
+
+@section('content')
+    <!-- MAIN CONTENT-->
+			<div class="main-content">
+				<h1>This is test page</h1>
+			</div>
+@endsection
